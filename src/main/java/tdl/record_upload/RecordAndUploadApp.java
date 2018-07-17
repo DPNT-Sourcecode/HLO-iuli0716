7e0f3b9abd66471fce4b61464d69bd02ab28fe9c
sbx_user1060 <sbx_user1060@ip-10-11-200-115.eu-west-2.compute.internal>
2018-07-17 20:25:27 +0000

    Tue Jul 17 20:25:27 UTC 2018

--- src/main/java/tdl/record_upload/RecordAndUploadApp.java
+++ src/main/java/tdl/record_upload/RecordAndUploadApp.java
@@ -137,6 +137,7 @@ public class RecordAndUploadApp {
         }
 
         externalEventServerThread.signalStop();
+        externalEventServerThread.join();
 
         // Finalise the upload and cancel tasks
         stopFileLogging();
